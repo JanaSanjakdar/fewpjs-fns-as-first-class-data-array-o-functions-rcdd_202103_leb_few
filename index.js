b20dd4c5296b3ca8e2ea28a844a51b7279bd92cc
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-14 10:07:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,23 +14,23 @@ function Unleash() {
   console.log("Unleash Byron the poodle");
 }*/
 //let dog= [dogName, dogBreed];
-function wakeDog(Xerox, Border Collie) {
+function wakeDog(Xerox, dogBreed) {
   console.log("Wake ${dogName} the ${dogBreed}");
   return "Wake ${dogName} the ${dogBreed}";
 }
-function leashDog(Boo Radley,Piddle) {
+function leashDog(Boo Radley,dogBreed) {
   console.log("Leash ${dogName} the ${dogBreed}");
   return "Leash ${dogName} the ${dogBreed}";
 }
-function walkToPark(Bunny, Labrador) {
+function walkToPark(Bunny, dogBreed) {
   console.log("Walk to the park with ${dogName} the ${dogBreed}");
   return "Walk to the park with ${dogName} the ${dogBreed}";
 }
-function throwFrisbee(Mary J. Blige, Papillon) {
+function throwFrisbee(Mary J. Blige, dogBreed) {
   console.log("Throw the frisbee for ${dogName} the ${dogBreed}");
   return "Throw the frisbee for ${dogName} the ${dogBreed}";
 }
-function Unleash(Byron, poodle) {
+function Unleash(Byron, dogBreed) {
   console.log("Unleash ${dogName} the ${dogBreed}");
   return "Unleash ${dogName} the ${dogBreed}";
 }
