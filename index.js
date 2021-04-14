e6128c37fd849b87522c93bbdd57c6664b2f3755
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-14 08:55:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,18 +16,23 @@ function Unleash() {
 
 function wakeDog(dogName, dogBreed) {
   console.log("Wake ${dogName} the ${dogBreed}");
+  return "Wake ${dogName} the ${dogBreed}";
 }
 function leashDog(dogName, dogBreed) {
   console.log("Leash ${dogName} the ${dogBreed}");
+  return "Leash ${dogName} the ${dogBreed}";
 }
 function walkToPark(dogName, dogBreed) {
   console.log("Walk to the park with ${dogName} the ${dogBreed}");
+  return "Walk to the park with ${dogName} the ${dogBreed}";
 }
 function throwFrisbee(dogName, dogBreed) {
   console.log("Throw the frisbee for ${dogName} the ${dogBreed}");
+  return "Throw the frisbee for ${dogName} the ${dogBreed}";
 }
 function Unleash(dogName, dogBreed) {
   console.log("Unleash ${dogName} the ${dogBreed}");
+  return "Unleash ${dogName} the ${dogBreed}";
 }
 
 let routine =[wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
